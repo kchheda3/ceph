781e6687424b090c1f35f5a44e2a6319c0a3b4dc
kchheda3 <kchheda3@bloomberg.net>
2024-09-25 09:52:52 -0400

    rgw/notification: Forward Topic & Notification creation request to master when notification_v2 enabled.
    Signed-off-by: kchheda3 <kchheda3@bloomberg.net>

--- src/rgw/rgw_rest_pubsub.cc
+++ src/rgw/rgw_rest_pubsub.cc
@@ -396,7 +396,9 @@ class RGWPSCreateTopicOp : public RGWOp {
 
 void RGWPSCreateTopicOp::execute(optional_yield y) {
   // master request will replicate the topic creation.
-  if (!driver->is_meta_master()) {
+  if (!driver->is_meta_master() &&
+      rgw::all_zonegroups_support(*s->penv.site,
+                                  rgw::zone_features::notification_v2)) {
     op_ret = rgw_forward_request_to_master(
         this, *s->penv.site, s->owner.id, &bl_post_body, nullptr, s->info, y);
     if (op_ret < 0) {
@@ -1255,7 +1257,9 @@ int RGWPSCreateNotifOp::verify_permission(optional_yield y) {
 }
 
 void RGWPSCreateNotifOp::execute(optional_yield y) {
-  if (!driver->is_meta_master()) {
+  if (!driver->is_meta_master() &&
+      rgw::all_zonegroups_support(*s->penv.site,
+                                  rgw::zone_features::notification_v2)) {
     op_ret = rgw_forward_request_to_master(
         this, *s->penv.site, s->owner.id, &data, nullptr, s->info, y);
     if (op_ret < 0) {
